7bb45c46c4389a919c12c5111e88ff8e5e311192
root <root@ip-172-31-2-31.us-west-2.compute.internal>
2021-07-10 09:48:36 +0000

    file permissions changed

---
+++ hostupdate.sql
@@ -0,0 +1,6 @@
+update system_setting set value='54.186.149.13' where key='FX_HOST';
+update system_setting set value='false' where key='FX_SSL';
+update system_setting set value='5672' where key='FX_PORT';
+update system_setting set value='2020-06-20-1836' where key='BOT_TAG';
+update org set billing_email='admin@apisecuriti.io' where billing_email='admin@apisecuriti.io';
+update users set email='admin@apisecuriti.io' where email='admin@apisecuriti.io';
